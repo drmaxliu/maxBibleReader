5afcad690925dfd6f18cdcec700df5e9fb1df6aa
Max Liu <dr.max.liu@gmail.com>
2013-05-25 05:49:32 -0700

    With BootStrap

---
+++ README.md
@@ -0,0 +1,6 @@
+#Web Design Learning with Ruby on Rails
+
+This is a first trial ....
+
+[*Hello !*](http://aiching.com)
+[Max Liu](http://aiching.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd2c9db2baae567c22c5e4244496bb009aaf292042883f43d1642d27a46c23fa7cfac5e1b4e1f695d0f8e3ecc8ac57cee3b6cd502ed19cfbef256fc748c8b74d9'
